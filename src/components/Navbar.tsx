97355534d9de16e36c2c1e60543c26b0892ba2c3
Alex <k8cchaha@gmail.com>
2024-02-20 22:18:33 +0800

    add route, bootstrap

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { Container, Navbar as NavbarBs } from 'react-bootstrap'
+
+export function Navbar() {
+  return <NavbarBs className="bg-white shadow-sm mb-3">
+    <Container>NAV</Container>
+  </NavbarBs>
+}
